68626a61eacf7bf8bcad165ac09c8cb573387cf6
bugszhou <bugszhou@outlook.com>
2021-01-14 16:41:07 +0800

    feat: 增加onShow

--- template/src/_app.js
+++ template/src/_app.js
@@ -1,7 +1,10 @@
+import { updateApp } from 'miniapp-utils';
+
 export default {
   onLaunch() {
   },
   onShow() {
+    updateApp();
   },
   onError(msg) {
     console.log(msg, ' -----> onError');
--- template/src/app.js
+++ template/src/app.js
@@ -1,10 +1,6 @@
 /**
  * 全局
  */
-import { updateApp } from 'miniapp-utils';
-import { getENV } from './utils/util';
 import app from './_app';
 
-updateApp();
-console.log(getENV());
 App(app);
